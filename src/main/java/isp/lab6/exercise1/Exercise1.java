bfced260d3de62ca569a05d056801c352f71b4c6
Radu Miron <radu.miron@aut.utcluj.ro>
2023-04-01 15:29:29 +0300

    lab 6 2023
    Signed-off-by: Radu Miron <radu.miron@aut.utcluj.ro>

--- src/main/java/isp/lab6/exercise1/Exercise1.java
+++ src/main/java/isp/lab6/exercise1/Exercise1.java
@@ -2,6 +2,5 @@ package isp.lab6.exercise1;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println("Test implementation here.");
     }
 }
--- src/main/java/isp/lab6/exercise4/Exercise4.java
+++
@@ -1,7 +0,0 @@
-package isp.lab6.exercise4;
-
-public class Exercise4 {
-    public static void main(String[] args) {
-
-    }
-}
